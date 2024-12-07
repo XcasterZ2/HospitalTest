b05048a0fa50ce1cea279753ed5a2bea322edf0a
Pornsawan Praman <185787855+XcasterZ2@users.noreply.github.com>
2024-12-07 09:50:52 +0700

    test

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist', 'lph-hisopd')));
+// เส้นทาง static files ให้ชี้ไปที่ 'dist/lph-hisopd/browser'
+app.use(express.static(path.join(__dirname, 'dist', 'lph-hisopd', 'browser')));
 
+// เส้นทางสำหรับจัดการทุก request ที่เหลือ
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'lph-hisopd', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'lph-hisopd', 'browser', 'index.html'));
 });
 
+// กำหนด port
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
