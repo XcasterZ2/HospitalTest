f15918901f3a5828d92efdb3f6470e0f52796a1b
Pornsawan Praman <185787855+XcasterZ2@users.noreply.github.com>
2024-12-08 16:15:59 +0700

    infomation-left

--- src/app/home-main/main-content/content-left/filtering-infomation-left/filtering-infomation-left.component.ts
+++ src/app/home-main/main-content/content-left/filtering-infomation-left/filtering-infomation-left.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './filtering-infomation-left.component.scss'
 })
 export class FilteringInfomationLeftComponent {
-
+  isTreatmentSelected: boolean = false;
 }
